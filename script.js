e248280cc86602dae841548be5d948d2e9f87917
ZGuinan <134339332+ZGuinan@users.noreply.github.com>
2023-07-30 21:57:29 +0000

    first

---
+++ script.js
@@ -0,0 +1,35 @@
+let pets = [
+    {name: 'Moo', age: 2, species: 'guineapig'},
+    {name: 'Rex', age: 1, species: 'dog'}, 
+    {name: 'Charles', age: 3, species: 'guineapig'},
+    {name: 'Jordan', age: 4, species: 'guineapig'},
+    {name: 'Alex', age: 3, species: 'guineapig'}
+]
+
+function petName(){ 
+    let name = prompt('What is your pets name?')
+    let age = prompt('What is ' + name + "'s age?")
+    let species = prompt("What species is " + name + '?')
+
+    let pet = {
+        name: name,
+        age: age,
+        species: species,
+    }
+
+    pets. PushManager(pet)
+}
+
+function showPets() {
+    alert(pet.name)
+
+}
+
+function removePet(){
+
+ 
+
+
+function findPet(){
+
+}
